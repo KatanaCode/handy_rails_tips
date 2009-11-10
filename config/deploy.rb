b7545444919658f8deff11da8dbf9eab6bfc58ad
Gavin Morrice <dr_gavin@hotmail.com>
2009-11-10 17:07:03 +0000

    upgraded to Rails 2.3.4

---
+++ config/deploy.rb
@@ -0,0 +1,33 @@
+set :application, "handy_rails_tips"
+set :deploy_to, "/root/public_html/handy_rails_tips/"
+set :domain,  "handyrailstips.com"
+ 
+role :app, "204.232.194.123"
+role :web, "204.232.194.123"
+role :db,  "204.232.194.123", :primary => true
+ 
+set :scm, "git"
+set :scm_verbose, true 
+set :repository,  "git@github.com:GavinM/handy_rails_tips.git"
+set :scm_user, "gavinM"
+set :scm_passphrase, "bounce"
+ 
+default_run_options[:pty] = true
+
+set :user, "root"
+set :runner, user
+ 
+namespace :deploy do
+  task :start, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+ 
+  task :stop, :roles => :app do
+    # Do nothing.
+  end
+ 
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "touch #{current_release}/tmp/restart.txt"
+  end
+end
