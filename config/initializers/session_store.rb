0a29b8bb61f24fef552eadd1fb61a483af1f92cc
Gavin Morrice <dr_gavin@hotmail.com>
2009-12-06 21:03:56 +0000

    updated code

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-config.action_controller.session = {
+ActionController::Base.session = {
   :session_key => '_tips_session',
   :secret      => 'ab25d67b0823ee1fdc00b17bd4ad6f4e25ea7688a39dd315933cb0065a5d3dac311230d52d1f311013a0466276c0747dbdd651709376acc87309ba6d54e77c13'
 }
