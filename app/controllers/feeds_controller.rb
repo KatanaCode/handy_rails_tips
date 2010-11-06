6f3bddd8dec26af07fb58bb891ddc40def0e42ff
Gavin Morrice <gavin@thinkersplayground.com>
2010-11-06 08:24:24 +0000

    fixed bug in feeds controller

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,15 +1,11 @@
 class FeedsController < ApplicationController
   session :off
   def sitemap
-    @tips = Tip.for_public.all :order => "updated_at DESC"
+    @tips = Tip.order("updated_at DESC")
   end
 
   def latest_tips
-    @tips = Tip.for_public.all :order => "updated_at DESC"
+    @tips = Tip.order("updated_at DESC")
   end
   
-  def comments
-    @tip      = Tip.find(params[:tip_id])
-    @comments = @tip.comments.all :order => "created_at DESC"
-  end
 end
